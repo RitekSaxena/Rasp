5180561614d104e589bd70a83de49a2f953ddcf9
Ritek Saxena <riteksaxena123@gmail.com>
2022-05-12 02:27:17 +0530

    commiting

---
+++ screens/BookSlot.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BookSlot = () => {
+
+
+    
+
+
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default BookSlot
+
+const styles = StyleSheet.create({})
